28b80b0b7e16b212e236603329151df0758c1a0d
Luiz Araújo <luizhbd@gmail.com>
2023-01-26 21:53:39 -0300

    Add create and delete fornecedor

--- src/TobeDev.FirstProject.Web/Controllers/FornecedoresController.cs
+++ src/TobeDev.FirstProject.Web/Controllers/FornecedoresController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TobeDev.FirstProject.Web.Data.Entity;
 using TobeDev.FirstProject.Web.Models.Fornecedores;
 
 namespace TobeDev.FirstProject.Web.Controllers
@@ -30,5 +31,58 @@ namespace TobeDev.FirstProject.Web.Controllers
 
             return View(listaDeFornecedoresViewModel);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFornecedor(FornecedorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Console.WriteLine(model.RazaoSocial);
+
+            Fornecedor fornecedor = MapViewModelToFornecedor(model);
+
+            if (model.Id > 0)
+                databaseContext.Fornecedores.Update(fornecedor);
+            else
+                await databaseContext.Fornecedores.AddAsync(fornecedor);
+
+            await databaseContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id > 0)
+            {
+                var fornecedor = await databaseContext.Fornecedores.FindAsync(id);
+
+                databaseContext.Fornecedores.Remove(fornecedor);
+                databaseContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        private Fornecedor MapViewModelToFornecedor(FornecedorViewModel fornecedorViewModel)
+        {
+            return new Fornecedor
+            {
+                RazaoSocial = fornecedorViewModel.RazaoSocial,
+                CNPJ = fornecedorViewModel.CNPJ,
+                Id = fornecedorViewModel.Id
+            };
+        }
+
     }
 }
--- src/TobeDev.FirstProject.Web/Models/Fornecedores/FornecedorViewModel.cs
+++ src/TobeDev.FirstProject.Web/Models/Fornecedores/FornecedorViewModel.cs
@@ -1,11 +1,12 @@
-﻿namespace TobeDev.FirstProject.Web.Models.Fornecedores
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TobeDev.FirstProject.Web.Models.Fornecedores
 {
     public class FornecedorViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Razão Social é obrigatório")]
         public string RazaoSocial { get; set; }
         public string CNPJ { get; set; }
-
-        public int VIEWMODEL { get; set; }
     }
 }
